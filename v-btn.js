c8b69eb38a896904e8db92d37b024c7186366c20
vyantagc <56681543+vyantagc@users.noreply.github.com>
2019-11-05 17:32:46 +0700

    Update v-btn.js

--- v-btn.js
+++ v-btn.js
@@ -1,3 +1,3 @@
 function gVar(){var n={};window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(r,i,o){n[i]=o});return n}
-function mKifrm(yb){document.getElementById("download_area").innerHTML = unescape('<iframe class=\"btn-download\"  sandbox=\"allow-scripts allow-same-origin\" layout=\"responsive\" frameborder=\"0\" scrolling=\"no\" src=\"https:\/\/vj-electronics.blogspot.com\/?a='+yb+'&t=multi\"><\/iframe>');}
+function mKifrm(yb){document.getElementById("download_area").innerHTML = unescape('<iframe sandbox=\"allow-scripts allow-same-origin\" frameborder=\"0\" scrolling=\"no\" src=\"https:\/\/vj-electronics.blogspot.com\/?a='+yb+'&t=multi\"><\/iframe>');}
 function doing(t,e){for(var n=document.getElementsByClassName(t),i=0;i<n.length;i++)n[i].addEventListener("click",function(){var t=this.getAttribute("data-vid");window.location.href=e+"?yb="+t})}
