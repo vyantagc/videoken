173d7bfd8b37f0cdd3bd346db592cafbef79f3f9
vyantagc <56681543+vyantagc@users.noreply.github.com>
2020-01-26 00:07:41 +0700

    Add files via upload

---
+++ v-btn-multi.js
@@ -0,0 +1,3 @@
+function gVar(){var n={};window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(r,i,o){n[i]=o});return n}
+function mKifrm(yb){document.getElementById("download_area").innerHTML = unescape('<iframe class=\"btn-download\"  sandbox=\"allow-scripts allow-same-origin\" layout=\"responsive\" frameborder=\"0\" scrolling=\"no\" src=\"https:\/\/v-btn.blogspot.com\/?a='+yb+'&t=mp3\"><\/iframe>');}
+function doing(t,e){for(var n=document.getElementsByClassName(t),i=0;i<n.length;i++)n[i].addEventListener("click",function(){var t=this.getAttribute("data-vid");window.location.href=e+"?yb="+t})}
